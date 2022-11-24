3f9d3337a2f1bc88c2861d857e444b010007facb
Cledson <cledson1996@gmail.com>
2022-11-23 22:01:15 -0300

    tirei_o_header_do_app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ResetGlobalStyle } from "./Assets/resetCss";
-import Headers from "./Components/Header";
 import Home from "./Pages/Home/Home";
 import Sing_in from "./Pages/Sing_in/sing_in";
 import { useState } from 'react';
@@ -13,8 +12,7 @@ function App() {
     <>
       <ResetGlobalStyle />
       <BrowserRouter>
-          <Headers />
-        <Routes>
+                 <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sing_in" element={<Sing_in setdados={setdados}/>} />
         </Routes>
