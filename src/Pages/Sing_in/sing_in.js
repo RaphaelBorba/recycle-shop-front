9a7c8f114ae7ae1ad1b883ac0afbf9c738f8ced1
Cledson <cledson1996@gmail.com>
2022-11-28 00:21:40 -0300

    carrinho_ok

--- src/Pages/Sing_in/sing_in.js
+++ src/Pages/Sing_in/sing_in.js
@@ -19,7 +19,6 @@ export default function Sing_in() {
             [name]: value,
         });
     };
-
     function autoriza() {
         setcarregando(["referencia"])
         let resposta = postLogin(login);
